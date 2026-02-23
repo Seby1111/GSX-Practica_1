dda49249bc01d02baa62d5e62c5907e7e88e7217
Eusebiu Boloc <eusebiu.boloc@estudiants.urv.cat>
2026-02-23 22:32:43 +0100

    afegir usuaris + ssh

--- script_1.sh
+++ script_1.sh
@@ -18,9 +18,43 @@ if [ $status -eq 127 ]; then
     fi
 fi
 
-echo "S'ha trobat sudo"
+echo "Cal actualitzar el sistema, voleu fer-ho? (y/n)"
 
+read resposta
 
-#sudo apt update && sudo apt upgrade -y
+if [ "$resposta" = "y" ]; then
+    sudo apt update && sudo apt upgrade -y
+else
+    echo "No s'ha actualitzat el sistema, no es poden executar les següents comandes"
+    exit 1
+fi
 
-#sudo apt install openssh-server -y
+echo "Afegim un nou usuari, voleu fer-ho? (y/n)"
+
+read resposta
+
+if [ "$resposta" = "y" ]; then
+    sudo useradd eusebiu
+    sudo passwd eusebiu
+else
+    echo "No s'ha afegit l'usuari 'eusebiu'"
+fi
+
+echo "Afegim un nou usuari, voleu fer-ho? (y/n)"
+
+read resposta
+
+if [ "$resposta" = "y" ]; then
+    sudo useradd alex
+    sudo passwd alex
+else
+    echo "No s'ha afegit l'usuari 'alex'"
+fi
+
+echo "Cal instal·lar el servidor SSH, voleu fer-ho? (y/n)"
+
+read resposta
+
+if [ "$resposta" = "y" ]; then
+    sudo apt install openssh-server -y
+fi
