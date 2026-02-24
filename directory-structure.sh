91e1e2af9769bc831cdca2f0d60497deec4612da
AlexRadu687 <gigagobrrr@gmail.com>
2026-02-24 19:21:44 +0100

    echos descriptivos para los 3 scripts finales aparte de añadido #!/bin/bash en todos

--- directory-structure.sh
+++ directory-structure.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+echo "Creant estructura de directoris per al projecte"
+
 mkdir admin
 
 cd admin
--- setup-verification.sh
+++ setup-verification.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 chmod +x script1.sh script2.sh
+echo "Ejecutando script1.sh: Errores se guardarán en script1_error"
 ./script1.sh 2>/dev/script1_error
+echo "Ejecutando script2.sh: Errores se guardarán en script2_error"
 ./script2.sh 2>/dev/script2_error
