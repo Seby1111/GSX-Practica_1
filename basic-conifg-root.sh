d685189a9510c485e00360916c81441c80bab6d2
Eusebiu Boloc <eusebiu.boloc@estudiants.urv.cat>
2026-02-26 13:12:04 +0100

    activades les actualitzacions automàtiques de seguretat

--- basic-conifg-root.sh
+++ basic-conifg-root.sh
@@ -5,7 +5,7 @@ add_user() {
         echo "[INFO] Creant usuari $1..."
         useradd -m "$1"
         echo "$1:milax" | chpasswd
-        change -d 0 "$1"
+        chage -d 0 "$1"
 
         echo "[INFO] Afegint $1 al grup sudo..."
         usermod -aG sudo "$1"
@@ -32,11 +32,14 @@ for user in "${USERS[@]}"; do
 done
 
 echo "[INFO] Actualitzant repositoris..."
-
 apt update
 
+echo "[INFO] Actualitzant paquets instal·lats..."
+apt upgrade -y
+
 echo "[INFO] Instal·lant paquets necessaris..."
-PACKAGES=("sudo" "git" "openssh-server")
+
+PACKAGES=("sudo" "git" "openssh-server" "unattended-upgrades" "apt-listchanges")
 
 for package in "${PACKAGES[@]}"; do
     install_if_missing "$package"
@@ -45,10 +48,36 @@ done
 echo "[INFO] Configuració inicial del servei SSH..."
 
 echo "[INFO] Canviant el port de SSH a 2222..."
-sed -i 's/.*Port 22.*/Port 2222/' /etc/ssh/sshd_config
+sed -i 's/^#\?Port .*/Port 2222/' /etc/ssh/sshd_config
 
 echo "[INFO] Deshabilitant l'accés directe de root a SSH..."
-sudo sed -i 's/.*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 
 systemctl enable ssh
 systemctl restart ssh
+
+echo "[OK] Configuració de SSH completada."
+
+echo "[INFO] Activant actualitzacions de seguretat automàtiques..."
+
+cat <<EOF > /etc/apt/apt.conf.d/50unattended-upgrades
+Unattended-Upgrade::Allowed-Origins {
+    "\${distro_id}:\${distro_codename}-security";
+    "\${distro_id}ESMApps:\${distro_codename}-apps-security";
+    "\${distro_id}ESM:\${distro_codename}-infra-security";
+};
+
+Unattended-Upgrade::Package-Blacklist {
+};
+EOF
+
+cat <<EOF > /etc/apt/apt.conf.d/20auto-upgrades
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+
+systemctl enable --now unattended-upgrades
+
+echo "[OK] Actualitzacions de seguretat automàtiques activades."
+
+echo "[OK] Configuració bàsica completada. Els usuaris creats són: ${USERS[*]}."
