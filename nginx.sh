775382dff8283b7184884a39e9c49d978161d35f
AlexRadu687 <gigagobrrr@gmail.com>
2026-02-26 11:04:44 +0100

    nginx avanzado un poco

--- nginx.sh
+++ nginx.sh
@@ -1,5 +1,22 @@
-systemctl list-units --type=service --all | grep nginx
-if [ $? -ne 0 ]; then
+#!/bin/bash
+
+# Aquest script comprova si Nginx està actiu i habilitat, i si no ho està, el posa en marxa i l'habilita. També comprova si Nginx està instal·lat i, si no ho està, mostra un missatge d'error.
+if [ sudo systemctl is-active nginx.service -eq 0 ]; then
+    echo "[!] Nginx no està actiu. Activant..."
+    sudo systemctl start nginx.service
+else
+    echo "[OK] Nginx està actiu."
+fi
+
+# Comprova si Nginx està habilitat i, si no ho està, l'habilita.
+if [ sudo systemctl is-enabled nginx.service -eq 0 ]; then
+    echo "[!] Nginx no està habilitat. Habilitant..."
+    sudo systemctl enable nginx.service
+else
+    echo "[OK] Nginx està habilitat."
+fi
+
+if ! [ -x "$(command -v nginx)" ]; then
     echo "[!] Nginx no està instal·lat. Executa el script de configuració bàsica abans d'executar aquest script."
     exit 1
 else
